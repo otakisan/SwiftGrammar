15fbcfb096b2f0439624bf3e08717a93d9c54df8
Takashi Ikeda <takashi@scorpius.local>
2014-09-07 11:54:13 +0900

    Initial Commit

---
+++ SwiftGrammar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftGrammar
+//
+//  Created by Takashi Ikeda on 2014/09/07.
+//  Copyright (c) 2014年 TI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
